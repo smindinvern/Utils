55b5f4e383a5ac74e85c94df14102524e66a9c46
Nickolas Lloyd <smindinvern@users.noreply.github.com>
2019-11-17 19:22:55 -0500

    Add a List module and Seq module

--- Utils.fs
+++ Utils.fs
@@ -1,18 +1,36 @@
-module Utils
+namespace smindinvern
+
+module Utils =
 
     open System
 
-    let fold' f = function
-    | (x::xs) -> List.fold f x xs
-    | _ -> raise <| new InvalidOperationException()
+    module List =
+        let fold' f = function
+            | (x::xs) -> List.fold f x xs
+            | _ -> raise <| new InvalidOperationException()
+
+        let inline cons (head: 'a) (tail: 'a list) = head::tail
+        let uncons = function
+            | [] -> raise <| InvalidOperationException()
+            | x::xs -> (x, xs)
+
+        let rec intersperse (a: 'a) (xs: 'a list) : 'a list =
+            List.foldBack (fun x xs' -> x::a::xs') xs []
+
+    module Seq =
+        let uncons xs =
+            if Seq.isEmpty xs then
+                raise <| InvalidOperationException()
+            else
+                (Seq.head xs, Seq.tail xs)
+
+        let fold' f xs =
+            let (head, tail) = uncons xs
+            Seq.fold f head tail
 
     let inline flip f x y = f y x
     let inline konst x _ = x
-    let inline cons (head: 'a) (tail: 'a list) = head::tail
     let mkPair x y = (x,y)
 
     let inline maybe (def: 'b) (f: 'a -> 'b) (a: 'a option) : 'b =
         Option.fold (fun _ -> f) def a
-
-    let rec intersperse (a: 'a) (xs: 'a list) : 'a list =
-        List.foldBack (fun x xs' -> x::a::xs') xs []
